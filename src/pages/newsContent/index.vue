383d1dad25e2ac9ccd5b63293664dbdcdb077599
baihuirong111@126.com <baihuirong111@126.com>
2018-12-17 20:24:05 +0800

    使用mpvue-wxParse解析小程序中的富文本

--- src/pages/newsContent/index.vue
+++ src/pages/newsContent/index.vue
@@ -1,72 +1,81 @@
 <template>
+    
+   <div>
+        <h2  class="title">{{list.title}}</h2>
 
-
-    <div>
-        
-        <h2 class="title">{{list.title}}</h2>
-        
-        <div class="content" v-html="list.content">
-
-
-        </div>
-
-    </div> 
-
-
+       
+  <div class="content">
+         <wxParse :content="list.content" />
+    </div>
+    </div>
 </template>
 
+
 <script>
+
+
+
+    import wxParse from 'mpvue-wxparse'
     export default{
 
         data(){
-            return{
+            return{                
                 list:[]
             }
         },
-        methods:{
-
+        components: {
+            wxParse
+        },
+        methods:{ 
+         
             requestData(aid){
 
                 var that=this;
 
                 var api='http://www.phonegap100.com/appapi.php?a=getPortalArticle&aid='+aid;
                 wx.request({
-                        url: api, // 仅为示例，并非真实的接口地址
-                        header: {
-                           'content-type': 'application/json' // 默认值
-                        },
-                        success(res) {
-                              console.log(res.data);
-                              that.list=res.data.result[0];
-                        }
-                    })
+                    url: api, //仅为示例，并非真实的接口地址               
+                    header: {
+                        'content-type': 'application/json' // 默认值
+                    },
+                    success: function(res) {
+                        console.log(res.data);
+
+                        that.list=res.data.result[0];
+                    }
+                })
+
             }
-           
-        }, 
-        onLoad:function(options) {
+        },
+        onLoad: function(options) {
+            
+           var aid=options.aid;
 
-               var aid=options.aid;
-
-               this.requestData(aid);
-           }
-
-      
+           this.requestData(aid);
+        }
     }
 </script>
 
 <style>
-
-.content{
-    line-height: 2;
-
-}
-.content img{
-    max-width:90%;
     
-    margin: 0 auto;
 
-}
+    @import url("~mpvue-wxparse/src/wxParse.css");
+    
+    .content {
+
+        line-height:2;
+
+        padding:10px;
+    }
+
+     .content  img{
+
+         max-width:90%;
+
+         margin:0 auto;
+     }
+     .wxParse{
+
+         
+     }
 </style>
-
-
-
